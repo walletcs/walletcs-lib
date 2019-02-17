9d1b02122add51091a25de44843c1ba3cbbc1b7f
@d.rozlomiy <d.rozlomiy@bioinfo.pl>
2019-02-17 10:50:17 +0100

    added index

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+import {EtherTransactionDecoder, EtherTransaction, EtherKeyPair} from '../src/ether/transactions';
+import {FileTransactionGenerator, KeyTool} from '../src/walletcs-ether';
+
+
+export default {
+    EtherKeyPair: EtherKeyPair,
+    EtherTransaction: EtherTransaction,
+    EtherTransactionDecoder: EtherTransactionDecoder,
+    FileTransactionGenerator: FileTransactionGenerator,
+    KeyTool: KeyTool
+}
