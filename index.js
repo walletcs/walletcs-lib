5f17b2455a94b93be5c782fbaa3f02866fe24568
@d.rozlomiy <d.rozlomiy@bioinfo.pl>
2019-02-17 11:52:58 +0100

    changed export

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import {EtherTransactionDecoder, EtherTransaction, EtherKeyPair} from './src/ether/transactions';
 import {FileTransactionGenerator, KeyTool} from './src/walletcs-ether';
 
-
-export default {
+module.exports = {
     EtherKeyPair: EtherKeyPair,
     EtherTransaction: EtherTransaction,
     EtherTransactionDecoder: EtherTransactionDecoder,
     FileTransactionGenerator: FileTransactionGenerator,
     KeyTool: KeyTool
-}
+};
