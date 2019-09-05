9a058fe65ac10ae33176e8893cff4915ac708156
EightyEighth <denisrozlomiy@gmail.com>
2019-09-05 19:35:28 +0300

    fixed nonce calculation

--- src/utils.js
+++ src/utils.js
@@ -22,7 +22,7 @@ export class ConverterEtherCSVToTxObject {
       let nonce = await provider.getTransactionCount(this._publicKey);
       let result = Papa.parse(this._csvFile);
       let paramsArray = [];
-      for (let i = 1; i < result.data.length; i += 1){
+      for (let i = 0; i < result.data.length; i += 1){
         let tx = {};
         tx['nonce'] = nonce + i;
         let row = result.data[i];
